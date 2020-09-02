80ac10afac1891e36e7cae6ab1ceb841db043d13
adeliaayu <adeliaayut53@gmail.com>
2020-09-02 14:41:50 +0700

    use web API to access data

--- app/src/main/java/com/dicoding/kotlin/myquote/MainActivity.kt
+++ app/src/main/java/com/dicoding/kotlin/myquote/MainActivity.kt
@@ -2,10 +2,75 @@ package com.dicoding.kotlin.myquote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getRandomQuote()
+    }
+
+    private fun getRandomQuote() {
+        progressBar.visibility = View.VISIBLE
+        val client = AsyncHttpClient()
+        val url = "https://programming-quotes-api.herokuapp.com/quotes/random"
+        client.get(url, object : AsyncHttpResponseHandler() {
+
+            // Jika koneksi sukses
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<Header>,
+                responseBody: ByteArray
+            ) {
+                progressBar.visibility = View.INVISIBLE
+
+                val result = String(responseBody)
+                Log.d(TAG, result)
+                try {
+                    val responsObjects = JSONObject(result)
+
+                    val quote = responsObjects.getString("en")
+                    val author = responsObjects.getString("author")
+
+                    tvQuote.text = quote
+                    tvAuthor.text = author
+                } catch (e: Exception) {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+
+            }
+
+            // Jika koneksi gagal
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<Header>,
+                responseBody: ByteArray,
+                error: Throwable
+            ) {
+                progressBar.visibility = View.VISIBLE
+
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error.message}"
+                }
+                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
     }
 }
